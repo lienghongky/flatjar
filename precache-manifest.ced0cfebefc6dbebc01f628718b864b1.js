fa0675ad85ca8230282270068a36e65d01daa4f6
hongky <hongkypcx.hp@gmail.com>
2020-10-19 01:02:46 +0700

    Updates

---
+++ precache-manifest.ced0cfebefc6dbebc01f628718b864b1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e672c30d73e602517bb5a3892b1df9",
+    "url": "/flatjar/index.html"
+  },
+  {
+    "revision": "915b173094fd61c0eb11",
+    "url": "/flatjar/static/css/2.abf6921b.chunk.css"
+  },
+  {
+    "revision": "8c794981cb52a08f798f",
+    "url": "/flatjar/static/css/main.ca931ae1.chunk.css"
+  },
+  {
+    "revision": "915b173094fd61c0eb11",
+    "url": "/flatjar/static/js/2.4da01035.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/flatjar/static/js/2.4da01035.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c794981cb52a08f798f",
+    "url": "/flatjar/static/js/main.521f9eab.chunk.js"
+  },
+  {
+    "revision": "dc16b8a2d117ad53af4d",
+    "url": "/flatjar/static/js/runtime-main.8318868b.js"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/flatjar/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/flatjar/static/media/element-icons.d2f69a92.woff"
+  },
+  {
+    "revision": "597f57fe42efdcf3389f4ee4ad5576f8",
+    "url": "/flatjar/static/media/teacher.597f57fe.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b66608a3d906550d5654d8c2644dbcac.js"
+  "/flatjar/precache-manifest.ced0cfebefc6dbebc01f628718b864b1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/flatjar/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
